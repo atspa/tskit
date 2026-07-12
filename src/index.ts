ccfdff308e37c9d77616c8be0cd7d12c05e72d70
Dominik Bošnjak <bosnjakinimod@gmail.com>
2026-07-12 05:34:35 +0200

    2026-07-12 05:34:34: semi-automated commit

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,6 @@
-<<<<<<< HEAD
-import fetchGSheetSchema from "./fetchGsheetSchema/index.ts";
-import objectSize from "./objectSize/index.ts";
-
-export { fetchGSheetSchema, objectSize };
-
-export default { fetchGSheetSchema, objectSize };
-=======
 import fetchGSheetSchena from "./fetchGsheetSchema/index.ts";
 import objectSize from "./objectSize/index.ts";
 
 export { fetchGSheetSchena, objectSize };
 
 export default { fetchGSheetSchena, objectSize };
->>>>>>> 13e8326eaea1b3b21fe402865b70d60d4cae6c8b

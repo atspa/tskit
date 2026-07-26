ebae6fdbee1b4dc973c4a53df4de8adb63d1a6d1
Dominik Bošnjak <bosnjakinimod@gmail.com>
2026-07-26 06:19:23 +0200

    added src/parse-csv

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import { fetchGSheetSchema } from "./fetchGsheetSchema/index.ts";
 import { objectSize } from "./objectSize/index.ts";
 import airtableAttachment from "./airtableAttachment/index.ts"
-export { fetchGSheetSchema, objectSize, airtableAttachment };
+import parseCsv from "./parse-csv/index.ts";
 
-export default { fetchGSheetSchema, objectSize, airtableAttachment };
+export { fetchGSheetSchema, objectSize, airtableAttachment, parseCsv };
+
+
+export default { fetchGSheetSchema, objectSize, airtableAttachment, parseCsv };

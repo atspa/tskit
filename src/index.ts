fa436aaf4fdb9b3f9a74d0b85596b0eeab1c8257
Dominik Bošnjak <bosnjakinimod@gmail.com>
2026-07-26 06:43:57 +0200

    added src/unarchive

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,22 @@ import { objectSize } from "./objectSize/index.ts";
 import airtableAttachment from "./airtableAttachment/index.ts"
 import parseCsv from "./parse-csv/index.ts";
 import extractLinks from "./extract-links/index.ts";
+import Unarchive from "./unarchive/index.ts";
 
-export { fetchGSheetSchema, objectSize, airtableAttachment, parseCsv, extractLinks };
+export {
+    fetchGSheetSchema,
+    objectSize,
+    airtableAttachment,
+    parseCsv,
+    extractLinks,
+    Unarchive
+};
 
-
-export default { fetchGSheetSchema, objectSize, airtableAttachment, parseCsv, extractLinks };
+export default {
+    fetchGSheetSchema,
+    objectSize,
+    airtableAttachment,
+    parseCsv,
+    extractLinks,
+    Unarchive
+};
